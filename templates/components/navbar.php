59052223643c526109f1c890f4922944ec88a8e4
Pablo Ferreiro <pferreiromero@gmail.com>
2022-11-17 19:24:10 +0100

    hotfix

--- templates/components/navbar.php
+++ templates/components/navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
+    <a href="<?=$this->url('/')?>" class="navbar-item">UMABot-ng</a>
+
     <a role="button" id="navbar-burger" class="navbar-burger" aria-label="menu" aria-expanded="false">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -9,7 +11,6 @@
 
   <div id="navbar-menu" class="navbar-menu">
     <div class="navbar-start">
-      <a href="/" class="navbar-item">UMABot-ng</a>
       <?php foreach($this->links() as $link): ?>
       <a class="navbar-item" href="<?=$this->url($link['endpoint'])?>"><?=$this->e($link['name'])?></a>
       <?php endforeach ?>
