d2ace62493c912098baa1e2bcd6e54b06e1cdfe9
Pablo Ferreiro <pferreiromero@gmail.com>
2022-11-19 10:25:05 +0100

    Desactivar moderación si verifico identidad

--- verification.sh
+++ verification.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ $# -eq 0 ]; then
-    echo "Necesitas enviar un modo"
+    echo "Uso: verification.sh MODE (on/off)"
     exit 1
 fi
 
@@ -12,11 +12,14 @@ if [ ! -f "$SCRIPT_DIR/.env" ]; then
 fi
 
 
-
 # Swap mode
 # TODO, maybe a better way to do this?
-if [ $1 = "enable" ]; then
+if [ $1 = "on" ]; then
+    # Turn on verification and disable moderation
     sed -i 's/APP_VERIFICATION=false/APP_VERIFICATION=true/g' "$SCRIPT_DIR/.env"
+    sed -i 's/APP_MODERATION=true/APP_MODERATION=false/g' "$SCRIPT_DIR/.env"
 else
+    # Turn on moderation and disable verification
+    sed -i 's/APP_MODERATION=false/APP_MODERATION=true/g' "$SCRIPT_DIR/.env"
     sed -i 's/APP_VERIFICATION=true/APP_VERIFICATION=false/g' "$SCRIPT_DIR/.env"
 fi
